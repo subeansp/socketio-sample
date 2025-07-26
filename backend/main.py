1792b63ab040b1524507b1b1644a8821e2dba916
Sota <bns18sp@gmail.com>
2025-07-26 11:31:18 +0900

    簡易的なSocket.IOのクライアントとサーバーの実装

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+import socketio
 
-app = FastAPI()
+sio = socketio.AsyncServer(
+    async_mode="asgi",
+    cors_allowed_origins=["http://localhost:5173"],
+)
+
+app = socketio.ASGIApp(sio)
 
 
-@app.get("/")
-def say_hello():
-    return {"message": "Hello from main.py!!!"}
+@sio.event
+async def connect(sid, environ):
+    print(f"クライアント接続: {sid}")
+
+
+@sio.event
+async def disconnect(sid):
+    print(f"クライアント切断: {sid}")
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,31 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
 const App = () => {
-  return <h1>socketio-sample</h1>;
+  useEffect(() => {
+    const newSocket = io("http://localhost:8000");
+
+    newSocket.on("connect", () => {
+      console.log(`サーバーに接続しました sid: ${newSocket.id}`);
+    });
+
+    newSocket.on("disconnect", () => {
+      console.log("サーバーから切断されました");
+    });
+
+    return () => {
+      console.log("Socket接続をクリーンアップ");
+      newSocket.close();
+    };
+  }, []);
+
+  return (
+    <form>
+      <h1>Socket.IO Reactクライアント</h1>
+      <input type="text" placeholder="メッセージ" />
+      <button type="submit">送信</button>
+    </form>
+  );
 };
 
 export default App;
